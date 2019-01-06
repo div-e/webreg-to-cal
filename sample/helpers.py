cff0da71454fa20357d5b0ac0cda884ad7e14a78
billyrrr <billrao@me.com>
2019-01-05 21:15:15 -0800

    Add helpers and tests for creating event

--- sample/helpers.py
+++ sample/helpers.py
@@ -1,4 +1,47 @@
 import datetime
+import re
+
+QUARTER_START_DATE = "2019-01-07"
+QUARTER_END_UTC_STR = "20190316T235959Z"
+DEFAULT_TIMEZONE = 'America/Los_Angeles'
+# Mapping days of the week: Sunday to 1, Monday to 2, ..., Saturday to 7
+DAYS_MAP = {
+    "WhateverThatIsSunday": 1,  # Since we do not know the string rep of Sunday in Webreg
+    "M": 2, 
+    "Tu": 3,
+    "W": 4,
+    "Th": 5, 
+    "F": 6,
+    "Sa": 7
+}
+
+BYDAYS_MAP = {
+    1: "SU",
+    2: "MO",
+    3: "TU",
+    4: "WE",
+    5: "TH",
+    6: "FR",
+    7: "SA"
+}
+
+DEFAULT_FREQ = "WEEKLY"
+
+def split_days(days: str) -> [str]:
+    """
+    Returns a list of strings from seperating days string by capital letters. 
+
+        ref: https://stackoverflow.com/questions/2277352/split-a-string-at-uppercase-letters
+        
+        re.findall('[A-Z][^A-Z]*', 'TheLongAndWindingRoad')
+        ['The', 'Long', 'And', 'Winding', 'Road']
+        
+        re.findall('[A-Z][^A-Z]*', 'ABC')
+        ['A', 'B', 'C']
+
+        :param days:str: 
+    """
+    return re.findall('[A-Z][^A-Z]*', days)
 
 def get_answer():
     """Get an answer."""
@@ -10,12 +53,109 @@ def get_offset(time_str: str) -> datetime.timedelta:
     offset_midnight: datetime.timedelta = t - midnight
     return offset_midnight
 
+def get_offset_pair(time_str: str):
+    time_many = time_str.split("-")
+    start_str: str = time_many[0]
+    end_str: str = time_many[1]
+    # ref: https://stackoverflow.com/questions/41308016/python-converting-time-format-1200a-to-24hour
+    start_offset = get_offset(start_str)
+    end_offset = get_offset(end_str)
+    return start_offset, end_offset
+
+def get_time_pair(start_offset: datetime.timedelta, end_offset: datetime.timedelta, quarter_start: datetime.datetime) -> (datetime.datetime, datetime.datetime) :
+    start_time = quarter_start + start_offset
+    end_time = quarter_start + end_offset
+    return (start_time, end_time)
+
+def get_summary(subject_course: str, row_type: str):
+    """
+    docstring here
+        :param subject_course:str: 
+        :param row_type:str: "LE" for lecture
+    """
+    return "{} {}".format(subject_course, row_type)
+
+def get_quarter_start(quarter_start_date: str) -> datetime.datetime: 
+    """
+    Returns quarter start day datetime
+        :param quarter_start_date:str: must be Monday of the quarter instruction start week
+    """
+    start_day_datetime = datetime.datetime.fromisoformat(quarter_start_date)
+    assert start_day_datetime.weekday() == 0
+    return start_day_datetime
+
+def get_first_day_of_section(quarter_start_date: str, first_day_num) -> datetime.datetime:
+    """
+    Returns datetime of first day of section 
+    Known issues: does not support Sunday as first day of meeting
+        :param quarter_start_date:str: Monday of the quarter instruction start week 
+        :param first_day_num: 1 if Sunday, 2 if Monday, ..., 6 if Sunday
+    """
+    quarter_start_monday = get_quarter_start(quarter_start_date)
+    offset_from_monday =  first_day_num - 2 # 0 for Monday
+    delta = datetime.timedelta(days=offset_from_monday)
+    start_day_datetime = quarter_start_monday + delta
+    return start_day_datetime
+
+def get_day_nums(days: str) -> []:
+    day_list = split_days(days) 
+    day_to_num = lambda a: DAYS_MAP[a] # M -> 2
+    day_nums: list = [ day_to_num(day) for day in day_list]
+    day_nums.sort()
+    return day_nums
+
+def get_first_meeting_day_num(days: str):
+    day_nums = get_day_nums(days)
+    first_day_num = day_nums[0]
+    return first_day_num
+
+def get_byday(days: str):
+    """
+    Returns BYDAY field value (MO,WE,FR) for building recurrence string. 
+        :param days:str: example: "MWF" for Monday, Wednesday, Friday
+    """
+    day_nums = get_day_nums(days)
+    num_to_bydays = lambda a: BYDAYS_MAP[a] # 2 -> Monday
+    bydays = [ num_to_bydays(day_num) for day_num in day_nums ]
+    byday_str = ",".join(bydays)
+    return byday_str
+
+def get_recurrence(days: str, until: str) -> [str]:
+    """
+    Returns the recurrence field that specified which days of a week do the section
+        occur and until when will the recurrence end 
+        https://developers.google.com/calendar/recurringevents
+
+        Sample return: list: ["RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR;UNTIL=20190316T235959Z"]
+
+        :param days:str: example: "MWF" for Monday, Wednesday, Friday
+        :param until:str: example: "20190316T235959Z"
+    """
+    freq = DEFAULT_FREQ
+    byday = get_byday(days)
+    return ["RRULE:FREQ={};BYDAY={};UNTIL={}".format(freq, byday, until)]
+
+def generate_event(subject_course, row: dict):
+    first_meeting_day_num = get_first_meeting_day_num(row["Days"])
+    start_day_datetime = get_first_day_of_section(QUARTER_START_DATE, first_meeting_day_num)
+    start_offset, end_offset = get_offset_pair(row["Time"])
+    start_time, end_time = get_time_pair(start_offset, end_offset, start_day_datetime)
+    # reccurrence_str = get_recurrence()
+
+    d = dict()
+    d["summary"] = get_summary(subject_course, row["Type"])
+    d["start"] = {
+        'dateTime': start_time.isoformat(),
+        'timeZone': DEFAULT_TIMEZONE,
+    }
+    d["end"] = {
+        'dateTime': end_time.isoformat(),
+        'timeZone': DEFAULT_TIMEZONE,
+    }
+    d["recurrence"] = get_recurrence(row["Days"], QUARTER_END_UTC_STR)
+    return d
+
 def parse_course(subject_course, rows:dict): 
     for row in rows:
-        time_str: str = row["Time"]
-        time_many = time_str.split("-")
-        start_str: str = time_many[0]
-        end_str: str = time_many[1]
-        # ref: https://stackoverflow.com/questions/41308016/python-converting-time-format-1200a-to-24hour
-        start_offset = get_offset(start_str)
-        end_offset = get_offset(end_str)
+        r = generate_event(subject_course, row)
+        raise NotImplementedError
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,17 +1,74 @@
 # -*- coding: utf-8 -*-
 
 from .context import sample
+import sample.helpers as helpers
 from sample.helpers import parse_course
 import json
+import datetime
 
 import unittest
 
 
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
+class TestHelper(unittest.TestCase):
 
-    def test_absolute_truth_and_meaning(self):
-        assert True
+    def test_generate_quarter_start(self):
+        result = helpers.get_quarter_start("2019-01-07")
+        expected_value = datetime.datetime(2019, 1, 7)
+
+        self.assertEquals(expected_value, result)
+
+    def test_summary(self):
+        result = helpers.get_summary("MATH 171A", "LE")
+        expected_value = 'MATH 171A LE'
+
+        self.assertEquals(expected_value, result)
+
+    def test_get_offset(self):
+        result = helpers.get_offset("1:50p")
+        expected_offset = datetime.timedelta(
+            hours=13, minutes=50)  # 13 hours 50 minutes
+
+        self.assertEquals(expected_offset, result)
+
+    def test_time_pair(self):
+        quarter_start = datetime.datetime(2019, 1, 5)
+        start_offset = datetime.timedelta(hours=13)  # 13 hours
+        end_offset = datetime.timedelta(
+            hours=13, minutes=50)  # 13 hours 50 minutes
+        result = helpers.get_time_pair(start_offset, end_offset, quarter_start)
+        expected_value = (datetime.datetime(2019, 1, 5, 13, 00),
+                          datetime.datetime(2019, 1, 5, 13, 50))
+        self.assertTupleEqual(result, expected_value)
+
+    def test_split_days(self):
+        result = helpers.split_days("TuTh")
+        expected_value = ["Tu", "Th"]
+
+        self.assertListEqual(result, expected_value)
+
+    def test_get_byday(self):
+        result = helpers.get_byday("TuTh")
+        expected_value = "TU,TH"
+        self.assertEquals(result, expected_value)
+
+    def test_get_recurrence(self):
+        result = helpers.get_recurrence("MWF", "20190316T235959Z")
+        expected_recurrence = ["RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR;UNTIL=20190316T235959Z"]
+
+        self.assertEqual(result, expected_recurrence)
+
+    def test_get_first_day_of_section_monday(self):
+        result = helpers.get_first_day_of_section("2019-01-07", 2) # Monday: 2nd day of week
+        expected_value = datetime.datetime(2019, 1, 7)
+
+        self.assertEquals(expected_value, result)
+
+    def test_get_first_day_of_section_tuesday(self):
+        result = helpers.get_first_day_of_section("2019-01-07", 3) # Tuesday: 3rd day of week
+        expected_value = datetime.datetime(2019, 1, 8)
+
+        self.assertEquals(expected_value, result)
+
 
 
 expected_event = {
@@ -23,11 +80,11 @@ expected_event = {
         # Instead of '2018-09-07T13:50:00-07:00'
         # 'America/Los_Angeles' has daylight saving, so we will not use
         #   dateTime with fixed offset
-        'dateTime': '2018-09-07T13:00:00',
+        'dateTime': '2019-01-07T13:00:00',
         'timeZone': 'America/Los_Angeles',
     },
     'end': {
-        'dateTime': '2018-09-07T13:50:00',
+        'dateTime': '2019-01-07T13:50:00',
         'timeZone': 'America/Los_Angeles',
     },
     'recurrence': [
@@ -61,9 +118,53 @@ class TestParseCourse(unittest.TestCase):
 
 
 class TestGenerateEvent(unittest.TestCase):
+    """
+    Tests generating event with a single row. 
+    """
 
-    def test_event(self):
-        pass
+    def setUp(self):
+        self.maxDiff = None
+        JSON_FILENAME = 'math171a.json'
+        with open('tests/jsons/{}'.format(JSON_FILENAME)) as json_file:
+            d = json.load(json_file)
+            self.subject_course = d["subjectCourse"]
+            self.rows = d["rows"]
+
+    def test_lecture_event(self):
+        result = helpers.generate_event(self.subject_course, self.rows[0])
+        self.assertDictEqual(result, expected_event)
+
+    def test_discussion_event(self):
+        row = self.rows[1]
+        print(row)
+        result = helpers.generate_event(self.subject_course, row)
+        expected_section_event = {
+            'summary': 'MATH 171A DI',
+            # 'location': '800 Howard St., San Francisco, CA 94103',
+            # 'location': "APM 2402",
+            # 'description': 'A chance to hear more about Google\'s developer products.',
+            'start': {
+                # Instead of '2018-09-07T13:50:00-07:00'
+                # 'America/Los_Angeles' has daylight saving, so we will not use
+                #   dateTime with fixed offset
+                'dateTime': '2019-01-10T21:00:00',
+                'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+                'dateTime': '2019-01-10T21:50:00',
+                'timeZone': 'America/Los_Angeles',
+            },
+            'recurrence': [
+                # MWF
+                # Instruction ends: Friday, March 15 (2019)
+                # Note that the time is in "Z" (which is "UTC")
+                #   we intend the end date to be 11:59pm one day later,
+                #   Saturday, March 16 (2019) in UTC Time
+                "RRULE:FREQ=WEEKLY;BYDAY=TH;UNTIL=20190316T235959Z"
+            ]
+        }
+
+        self.assertDictEqual(result, expected_section_event)
 
 
 if __name__ == '__main__':
